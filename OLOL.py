b30a6b8ab56f03154cfb817c50fdce7d2b5101f7
KaffeLatte <aalto.erik@gmail.com>
2015-09-01 17:14:22 +0200

    MVP backend is done

--- OLOL.py
+++ OLOL.py
@@ -6,13 +6,14 @@ import unicodedata
 class article:
     url = ""
     title = ""
-    id = 0
+    article_id = 0
     article_vector = None
+    mod_weight = 0.1
 
-    def __init__(self, url, title, id):
+    def __init__(self, url, title, article_id):
         self.url = url
         self.title = title
-        self.id = id
+        self.article_id = article_id
 
         # create 1000 dimensional row vector, with entries between 0 and 1
         random_vector = np.random.rand(1, 1000)
@@ -21,86 +22,110 @@ class article:
         vec_sum = np.sum(random_vector)
         self.article_vector = random_vector / vec_sum
 
+    # return the article vector in a json friendly format
     def as_dict(self):
 
         retValue = dict()
         retValue['url'] = self.url
         retValue['title'] = self.title
-        retValue['id'] = self.id
+        retValue['article_id'] = self.article_id
         retValue['article_vector'] = np.array(self.article_vector).tolist()
 
         return retValue
 
-def getHN_stories(article_limit):
-    hn = HackerNews()
+    # If a user with user_vector as user vector clicks this article then modify the article vector as follows
+    def mod_article_vector(self, user_vector):
+        user_vector = self.mod_weight * user_vector
 
-    articles_to_retrieve = int(article_limit*1.5)
-    top_story_ids = hn.top_stories(limit=articles_to_retrieve)
+        tmp_vector = self.article_vector + user_vector
+        tmp_vector = tmp_vector / (1 + self.mod_weight)
 
-    stories = []
-    for story_id in top_story_ids:
-        stories.append(hn.get_item(story_id))
+        self.article_vector = tmp_vector
 
-    return stories
-
-def HNstories_toArtList(stories, article_limit):
-    articles = []
-    counter = 0
-
-    for x in stories:
-        #print x
-        if counter == article_limit:
-            break
-
-        unicode_url = x.url
-        unicode_title = x.title
-
-        str_url = unicode_url.encode('ascii', 'ignore')
-        str_title = unicode_title.encode('ascii', 'ignore')
-
-        if str_url != "":
-            new_article = article(str_url, str_title, x.item_id)
-            articles.append(new_article)
-            counter += 1
-
-
-    return articles
-
-def convertArticlesToArtDict(articles, cached_articles):
-    new_cached_articles = dict()
-
-    for art in articles:
-        if art.url in cached_articles:
-            new_cached_articles[art.id] = cached_articles[art.id]
-        else:
-            new_cached_articles[art.id] = art
-
-    return new_cached_articles
-
-
-def mainScript():
-    article_limit = 30
+class pollArticles:
     cached_articles = dict()
+    article_limit = 2
 
-    hn_stories = getHN_stories(article_limit)
-    hn_articles = HNstories_toArtList(hn_stories, article_limit)
-    cached_articles = convertArticlesToArtDict(hn_articles, cached_articles)
+    def getHN_stories(self, article_limit):
+        hn = HackerNews()
 
-    article_keys = cached_articles.keys()
+        articles_to_retrieve = int(article_limit*1.5)
+        top_story_ids = hn.top_stories(limit=articles_to_retrieve)
 
-    article_list = []
-    for x in article_keys:
-        article_object = cached_articles[x]
-        article_list.append(article_object.as_dict())
+        stories = []
+        for story_id in top_story_ids:
+            stories.append(hn.get_item(story_id))
 
-    with open('top_articles.json', 'w') as outfile:
-        json.dump(article_list, outfile)
+        return stories
 
-'''
-    for x in l:
+    def HNstories_toArtList(self, stories, article_limit):
+        articles = []
+        counter = 0
+
+        for x in stories:
+            #print x
+            if counter == article_limit:
+                break
+
+            unicode_url = x.url
+            unicode_title = x.title
+
+            str_url = unicode_url.encode('ascii', 'ignore')
+            str_title = unicode_title.encode('ascii', 'ignore')
+
+            if str_url != "":
+                new_article = article(str_url, str_title, x.item_id)
+                articles.append(new_article)
+                counter += 1
+
+
+        return articles
+
+    def convertArticlesToArtDict(self, articles, cached_articles):
+        new_cached_articles = dict()
+
+        for art in articles:
+            if art.url in cached_articles:
+                new_cached_articles[art.article_id] = cached_articles[art.article_id]
+            else:
+                new_cached_articles[art.article_id] = art
+
+        return new_cached_articles
+
+
+    def poll_articles(self):
+        hn_stories = self.getHN_stories(self.article_limit)
+        hn_articles = self.HNstories_toArtList(hn_stories, self.article_limit)
+        self.cached_articles = self.convertArticlesToArtDict(hn_articles, self.cached_articles)
+
+        self.save_articles_to_json()
+
+    def save_articles_to_json(self):
+        article_keys = self.cached_articles.keys()
+
+        article_list = []
+        for x in article_keys:
+            article_object = self.cached_articles[x]
+            article_list.append(article_object.as_dict())
+
         with open('top_articles.json', 'w') as outfile:
-            article_object = cached_articles[x]
-            json.dump(article_object.as_dict(), outfile)
-'''
+            json.dump(article_list, outfile)
 
-mainScript()
+    def receive_user_action(self, user_vector, article_id):
+        article_to_mod = self.cached_articles[article_id]
+        article_to_mod.mod_article_vector(user_vector)
+
+        self.cached_articles[article_id] = article_to_mod
+        self.save_articles_to_json()
+
+
+
+
+backEnd = pollArticles()
+backEnd.poll_articles()
+
+# test case
+uv = np.zeros((1,1000))
+uv[0,999] = 1
+vvar = 10152735
+backEnd.receive_user_action(uv, vvar)
